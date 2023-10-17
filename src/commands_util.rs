86be16fff21af01ed4ea7beee1068aecb1676d27
YvesCB <y.bruckmayer@gmail.com>
2023-10-17 22:07:50 +0200

    pagination troubles

--- src/commands_util.rs
+++ src/commands_util.rs
@@ -1,4 +1,8 @@
-use poise::serenity_prelude::{ButtonStyle, ReactionType};
+use std::time::Duration;
+
+use poise::serenity_prelude::{
+    ButtonStyle, CollectComponentInteraction, Embed, InteractionResponseType, ReactionType,
+};
 
 use crate::types::*;
 
@@ -32,61 +36,74 @@ You can edit your message to the bot and the bot will edit its response.",
     Ok(())
 }
 
-/// A test for paging
-#[poise::command(slash_command)]
-pub async fn page_test(ctx: Context<'_>) -> Result<(), Error> {
-    let mut page = 1;
+/// Util function for creating pages that can be flipped through
+pub async fn page_test(ctx: Context<'_>, embeds: Vec<Embed>) -> Result<(), Error> {
+    // Define some unique identifiers for the navigation buttons
+    let ctx_id = ctx.id();
+    let prev_button_id = format!("{}prev", ctx_id);
+    let next_button_id = format!("{}next", ctx_id);
+
+    let mut page = 0;
+    let mut cur_embed = embeds.get(0).unwrap();
 
     let reply = ctx
         .send(|m| {
-            m.content(&page.to_string()).components(|c| {
+            m.content(page.to_string()).components(|c| {
                 c.create_action_row(|r| {
-                    r.create_button(|b| {
-                        b.custom_id("page.prev")
-                            .style(ButtonStyle::Primary)
-                            .disabled(match page {
-                                1 => true,
-                                _ => false,
-                            })
-                            .emoji(ReactionType::Unicode("⬅️".to_string()))
-                    })
-                    .create_button(|b| {
-                        b.custom_id("page.next")
-                            .style(ButtonStyle::Primary)
-                            .disabled(match page {
-                                3 => true,
-                                _ => false,
-                            })
-                            .emoji(ReactionType::Unicode("➡️".to_string()))
-                    })
+                    r.create_button(|b| b.custom_id(&prev_button_id).disabled(true).emoji('◀'))
+                        .create_button(|b| b.custom_id(&next_button_id).disabled(false).emoji('▶'))
                 })
             })
         })
         .await?;
 
-    while let Some(interaction) = reply
-        .message()
-        .await?
-        .await_component_interaction(ctx)
-        .author_id(ctx.author().id)
+    // Loop through incoming interactions with the navigation buttons
+    while let Some(interaction) = CollectComponentInteraction::new(ctx)
+        // We defined our button IDs to start with `ctx_id`. If they don't, some other command's
+        // button was pressed
+        .filter(move |press| press.data.custom_id.starts_with(&ctx_id.to_string()))
+        // Timeout after 10 min
+        .timeout(Duration::from_secs(600))
         .await
     {
-        let pressed_button_id = &interaction.data.custom_id;
+        let mut disabled_next = false;
+        let mut disabled_prev = false;
+        if interaction.data.custom_id == next_button_id {
+            page += 1;
+            if page == 3 {
+                disabled_next = true;
+            }
+        } else if interaction.data.custom_id == prev_button_id {
+            page -= 1;
+            if page == 1 {
+                disabled_prev = true;
+            }
+        } else {
+            continue;
+        }
 
-        match &**pressed_button_id {
-            "page.prev" => {
-                page -= 1;
-                reply.edit(ctx, |b| b.content(&page.to_string())).await?;
-            }
-            "page.next" => {
-                page += 1;
-                reply.edit(ctx, |b| b.content(&page.to_string())).await?;
-            }
-            _ => {}
-        };
+        interaction
+            .create_interaction_response(ctx, |b| {
+                b.kind(InteractionResponseType::UpdateMessage)
+                    .interaction_response_data(|b| {
+                        b.content(page).components(|c| {
+                            c.create_action_row(|r| {
+                                r.create_button(|b| {
+                                    b.custom_id(&prev_button_id)
+                                        .disabled(disabled_prev)
+                                        .emoji('◀')
+                                })
+                                .create_button(|b| {
+                                    b.custom_id(&next_button_id)
+                                        .disabled(disabled_next)
+                                        .emoji('▶')
+                                })
+                            })
+                        })
+                    })
+            })
+            .await?;
     }
-
-    reply.edit(ctx, |b| b.components(|f| f).content("")).await?;
 
     Ok(())
 }
