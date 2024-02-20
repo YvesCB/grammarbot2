2babe42702a23de76a6799182c9314e7b98c212c
YvesCB <y.bruckmayer@gmail.com>
2024-02-20 13:52:43 +0100

    migrated to poise 0.6, refactored pagination

--- src/embed_tools.rs
+++ src/embed_tools.rs
@@ -1,168 +1,126 @@
-use std::time::Duration;
-
-use poise::serenity_prelude::{CollectComponentInteraction, CreateEmbed, InteractionResponseType};
+use poise::serenity_prelude as serenity;
 
 use crate::types::*;
 
-pub async fn paginate_with_embeds(ctx: Context<'_>, embeds: Vec<CreateEmbed>) -> Result<(), Error> {
+async fn paginage_generic(
+    ctx: Context<'_>,
+    texts_embeds: (Option<Vec<String>>, Option<Vec<serenity::CreateEmbed>>),
+) -> Result<(), Error> {
     // Define some unique identifiers for the navigation buttons
     let ctx_id = ctx.id();
     let prev_button_id = format!("{}prev", ctx_id);
     let next_button_id = format!("{}next", ctx_id);
 
     let mut page = 0;
-    let last_page = embeds.len() - 1;
+    let last_page = match texts_embeds {
+        (Some(texts), None) => texts.len() == page,
+        (None, Some(embeds)) => embeds.len() == page,
+        _ => false,
+    };
 
-    ctx.send(|m| {
-        m.embed(|b| {
-            *b = embeds[page].to_owned();
-            b
-        })
-        .components(|c| {
-            c.create_action_row(|r| {
-                r.create_button(|b| b.custom_id(&prev_button_id).disabled(true).emoji('◀'))
-                    .create_button(|b| {
-                        b.custom_id(&next_button_id)
-                            .disabled(page == last_page)
-                            .emoji('▶')
-                    })
-            })
-        })
-    })
-    .await?;
+    let components = serenity::CreateActionRow::Buttons(vec![
+        serenity::CreateButton::new(prev_button_id)
+            .style(serenity::ButtonStyle::Primary)
+            .emoji('◀')
+            .disabled(true),
+        serenity::CreateButton::new(next_button_id)
+            .style(serenity::ButtonStyle::Primary)
+            .emoji('▶')
+            .disabled(page == last_page),
+    ]);
+    let builder = match texts_embeds {
+        (Some(texts), None) => poise::CreateReply::default()
+            .content(texts[page])
+            .components(components),
+        (None, Some(embeds)) => poise::CreateReply::default()
+            .embed(embeds[page])
+            .components(components),
+        _ => poise::CreateReply::default(),
+    };
 
-    // Loop through incoming interactions with the navigation buttons
-    while let Some(interaction) = CollectComponentInteraction::new(ctx)
-        // We defined our button IDs to start with `ctx_id`. If they don't, some other command's
-        // button was pressed
-        .filter(move |press| press.data.custom_id.starts_with(&ctx_id.to_string()))
-        // Timeout after 10 min
-        .timeout(Duration::from_secs(600))
-        .await
-    {
-        let mut disabled_next = false;
-        let mut disabled_prev = false;
-        if interaction.data.custom_id == next_button_id {
-            page += 1;
-            if page == last_page {
-                disabled_next = true;
+    let reply = ctx.send(builder).await?;
+
+    let interactions = reply
+        .message()
+        .await?
+        .await_component_interaction(ctx)
+        .author_id(ctx.author().id)
+        .custom_ids(vec![prev_button_id, next_button_id])
+        .timeout(60)
+        .stream()
+        .await;
+
+    while let Some(interaction) = interactions.poll_next() {
+        let pressed_button_id = match &interaction {
+            Some(m) => &m.data.custom_id,
+            None => {
+                ctx.say("Didn't interact in time.").await?;
+                return Ok(());
             }
-        } else if interaction.data.custom_id == prev_button_id {
-            page -= 1;
-            if page == 0 {
-                disabled_prev = true;
+        };
+        match pressed_button_id {
+            prev_button_id => {
+                page -= 1;
+                let disable_prev = page == 0;
+                let edit_components = serenity::CreateActionRow::Buttons(vec![
+                    serenity::CreateButton::new(prev_button_id)
+                        .style(serenity::ButtonStyle::Primary)
+                        .emoji('◀')
+                        .disabled(disable_prev),
+                    serenity::CreateButton::new(next_button_id)
+                        .style(serenity::ButtonStyle::Primary)
+                        .emoji('▶'),
+                ]);
+                let edit_builder = match texts_embeds {
+                    (Some(texts), None) => poise::CreateReply::default()
+                        .content(texts[page])
+                        .components(components),
+                    (None, Some(embeds)) => poise::CreateReply::default()
+                        .embed(embeds[page])
+                        .components(components),
+                    _ => poise::CreateReply::default(),
+                };
+                reply.edit(ctx, builder).await?;
             }
-        } else {
-            continue;
+            next_button_id => {
+                page += 1;
+                let disable_next = page == last_page;
+                let edit_components = serenity::CreateActionRow::Buttons(vec![
+                    serenity::CreateButton::new(prev_button_id)
+                        .style(serenity::ButtonStyle::Primary)
+                        .emoji('◀'),
+                    serenity::CreateButton::new(next_button_id)
+                        .style(serenity::ButtonStyle::Primary)
+                        .emoji('▶')
+                        .disable(disable_next),
+                ]);
+                let edit_builder = match texts_embeds {
+                    (Some(texts), None) => poise::CreateReply::default()
+                        .content(texts[page])
+                        .components(components),
+                    (None, Some(embeds)) => poise::CreateReply::default()
+                        .embed(embeds[page])
+                        .components(components),
+                    _ => poise::CreateReply::default(),
+                };
+                reply.edit(ctx, builder).await?;
+            }
         }
-
-        interaction
-            .create_interaction_response(ctx, |b| {
-                b.kind(InteractionResponseType::UpdateMessage)
-                    .interaction_response_data(|b| {
-                        b.embed(|b| {
-                            *b = embeds[page].to_owned();
-                            b
-                        })
-                        .components(|c| {
-                            c.create_action_row(|r| {
-                                r.create_button(|b| {
-                                    b.custom_id(&prev_button_id).disabled(true).emoji('◀')
-                                })
-                                .create_button(|b| {
-                                    b.custom_id(&next_button_id).disabled(false).emoji('▶')
-                                })
-                            })
-                        })
-                        .components(|c| {
-                            c.create_action_row(|r| {
-                                r.create_button(|b| {
-                                    b.custom_id(&prev_button_id)
-                                        .disabled(disabled_prev)
-                                        .emoji('◀')
-                                })
-                                .create_button(|b| {
-                                    b.custom_id(&next_button_id)
-                                        .disabled(disabled_next)
-                                        .emoji('▶')
-                                })
-                            })
-                        })
-                    })
-            })
-            .await?;
     }
 
     Ok(())
 }
 
 #[allow(dead_code)]
-pub async fn paginate_with_text(ctx: Context<'_>, texts: Vec<String>) -> Result<(), Error> {
+pub async fn paginate_with_embeds(
+    ctx: Context<'_>,
+    embeds: Vec<serenity::CreateEmbed>,
+) -> Result<(), Error> {
     // Define some unique identifiers for the navigation buttons
-    let ctx_id = ctx.id();
-    let prev_button_id = format!("{}prev", ctx_id);
-    let next_button_id = format!("{}next", ctx_id);
+    paginage_generic(ctx, (None, Some(embeds))).await?;
+}
 
-    let mut page = 0;
-    let last_page = texts.len() - 1;
-
-    ctx.send(|m| {
-        m.content(&texts[page]).components(|c| {
-            c.create_action_row(|r| {
-                r.create_button(|b| b.custom_id(&prev_button_id).disabled(true).emoji('◀'))
-                    .create_button(|b| b.custom_id(&next_button_id).disabled(false).emoji('▶'))
-            })
-        })
-    })
-    .await?;
-
-    // Loop through incoming interactions with the navigation buttons
-    while let Some(interaction) = CollectComponentInteraction::new(ctx)
-        // We defined our button IDs to start with `ctx_id`. If they don't, some other command's
-        // button was pressed
-        .filter(move |press| press.data.custom_id.starts_with(&ctx_id.to_string()))
-        // Timeout after 10 min
-        .timeout(Duration::from_secs(600))
-        .await
-    {
-        let mut disabled_next = false;
-        let mut disabled_prev = false;
-        if interaction.data.custom_id == next_button_id {
-            page += 1;
-            if page == last_page {
-                disabled_next = true;
-            }
-        } else if interaction.data.custom_id == prev_button_id {
-            page -= 1;
-            if page == 0 {
-                disabled_prev = true;
-            }
-        } else {
-            continue;
-        }
-
-        interaction
-            .create_interaction_response(ctx, |b| {
-                b.kind(InteractionResponseType::UpdateMessage)
-                    .interaction_response_data(|b| {
-                        b.content(&texts[page]).components(|c| {
-                            c.create_action_row(|r| {
-                                r.create_button(|b| {
-                                    b.custom_id(&prev_button_id)
-                                        .disabled(disabled_prev)
-                                        .emoji('◀')
-                                })
-                                .create_button(|b| {
-                                    b.custom_id(&next_button_id)
-                                        .disabled(disabled_next)
-                                        .emoji('▶')
-                                })
-                            })
-                        })
-                    })
-            })
-            .await?;
-    }
-
-    Ok(())
+#[allow(dead_code)]
+pub async fn paginate_with_text(ctx: Context<'_>, texts: Vec<String>) -> Result<(), Error> {
+    paginage_generic(ctx, (Some(texts), None)).await?;
 }
